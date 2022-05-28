d95a10cf8ac9bf7f78eb27400f062102bf2bc10d
mabrursatori <mabrursatori@gmail.com>
2022-05-28 07:01:30 +0700

    proses

--- src/components/common/heading/Header.jsx
+++ src/components/common/heading/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Head from './Head'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
     <Head/>
+    <header>
+      <nav className='flexSB'>
+        <ul className='flexSB'>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/team">Team</Link></li>
+          <li><Link to="/pricing">Journal</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <div className="start">
+          <div className="button">GET CERTIFICATE</div>
+        </div>
+      </nav>
+    </header>
     </>
   )
 }
